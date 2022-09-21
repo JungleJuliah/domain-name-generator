abd0421edbc95d9550cfdd6b8005640355dc2f4c
JungleJuliah <helenachamorroga@gmail.com>
2022-09-21 16:53:46 +0000

    este codigo genera un nombre de domain aleatorio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+const pronoun = ["your", "the", "it", "my", "go"];
+const adje = ["super", "economic", "easy", "smart", "best"];
+const noun = ["pet", "health", "electronics", "finds", "mart"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+// let randpron = pronoun[(Math.floor(Math.random()*5)+1)]
+// let randadje = adje[(Math.floor(Math.random()*5)+1)]
+// let randnoun = noun[(Math.floor(Math.random()*5)+1)]
+// NO NECESITO ALEATORIEDAD --> NECESITO EXHAUSTIVIDAD
+function randname() {
+  const domainlist = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adje.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        let newname = `${pronoun[i]}${adje[j]}${noun[k]}.com`;
+        domainlist.push(newname);
+      }
+    }
+  }
+  return domainlist;
+}
+console.log(randname());
